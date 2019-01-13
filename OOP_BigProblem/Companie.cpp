1d3bad4d0c358fd14def669ef4098d4424360b4b
Dumitru Cristina-Nicoleta <crs_nikoll23@yaho.com>
2019-01-14 00:10:38 +0200

    m

--- OOP_BigProblem/Companie.cpp
+++ OOP_BigProblem/Companie.cpp
@@ -2,11 +2,11 @@
 
 Companie::Companie() {
 	angajat = new Angajat*[5];
-	angajat[0] = new Manager("Ghera", "Alexandur", "012547896321", "87435DSFHSF", "manager");
-	angajat[1] = new Programator("Popescu", "Darius", "215487963254", "dsdgsf8767", "Proiect 1", "12.10.2018", 1, "20.10.2018", "programator");
+	angajat[0] = new Manager("Ghera", "Alexandru", "012547896321", "87435DSFHSF", "manager");
+	angajat[1] = new AngajatVanzari("Delcea", "Marius", "864565478325", "sgsf5466Hf", "produs2", 234.45, "angajat vanzari");
 	angajat[2] = new AngajatVanzari("Diaconu", "Alexandru", "654873219865", "645jgtkvb0", "produs1", 10.00, "angajat vanzari");
 	angajat[3] = new Programator("Guran", "Alin", "214876549320", "54fsghHJ54", "Proiectul 1", "12.10.2018", 1, "29.10.2018", "programator");
-	angajat[4] = new AngajatVanzari("Delcea", "Marius", "864565478325", "sgsf5466Hf", "produs2", 234.45, "angajat vanzari");
+	angajat[4] = new Programator("Popescu", "Darius", "215487963254", "dsdgsf8767", "Proiect 1", "12.10.2018", 1, "20.10.2018", "programator");
 	
 }
 
